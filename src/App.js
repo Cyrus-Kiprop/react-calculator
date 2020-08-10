cbdd771ae454c05a25494add3014d7c8e1a6ccfd
kip <cyruskiprop254@gmail.com>
2020-08-11 01:40:45 +0300

    :art: eslint and stylint files
    fix eslint and styling errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import logo from "./calculator-96.png";
-import "./App.css";
+import React from 'react';
+import logo from './calculator-96.png';
+import './App.css';
 
 function App() {
   return (
