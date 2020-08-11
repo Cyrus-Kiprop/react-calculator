b61188d196d4962359ec1f7288a41cc66d902f3c
kip <cyruskiprop254@gmail.com>
2020-08-12 01:18:15 +0300

    :fire: Functional Components
    Convert classical components into functional components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // eslint-disable-next-line react/prefer-stateless-function
-export default class Button extends React.Component {
-  render() {
-    const { btnName } = this.props;
-    return <div>{btnName}</div>;
-  }
-}
+const Button = ({ btnName }) => <div>{btnName}</div>;
 
 Button.propTypes = {
   btnName: PropTypes.string.isRequired,
 };
+
+export default Button;
