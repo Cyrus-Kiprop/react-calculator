d76f65396de6e89c839f4f29fc0a9d5a516f571d
kip <cyruskiprop254@gmail.com>
2020-08-12 16:59:17 +0300

    update the divide symbol form '/' to  '÷'

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -5,7 +5,7 @@ import Button from './Button.jsx';
 
 const ButtonPanel = () => {
   const group = {
-    one: ['AC', '+/-', '%', '/'],
+    one: ['AC', '+/-', '%', '÷'],
     two: ['7', '8', '9', 'X'],
     three: ['4', '5', '6', '-'],
     four: ['1', '2', '3', '+'],
@@ -15,7 +15,7 @@ const ButtonPanel = () => {
   const color = ['#F5913E', '#EOEOEO'];
 
   const styleColor = (btnName) =>
-    ['/', 'X', '-', '+', '='].includes(btnName) ? color[0] : color[1];
+    ['÷', 'X', '-', '+', '='].includes(btnName) ? color[0] : color[1];
 
   return (
     <div className="calculator-board">
